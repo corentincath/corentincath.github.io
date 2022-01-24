0df215efd1bbe713a107c188c6dcdbe357e3349c
Corentin <aazertypher@gmail.com>
2022-01-24 15:28:24 +0100

    Add HR

--- assets/bundle.js
+++ assets/bundle.js
@@ -6,9 +6,11 @@ import gsap from 'gsap';
 
 $(document).ready(function() {
     const title_span = document.querySelectorAll('h1 span');
+    const sub_title = document.querySelectorAll('h3 span');
 
-    const tl = gsap.timeline({repeat: 2, repeatDelay: 1, delay: 1});
+    const tl = gsap.timeline({delay: 1});
     tl.to(title_span[0], {y:0, duration: 1});
     tl.to(title_span[1], {y:0, duration: 1});
+    tl.to(sub_title, {x:0, duration: 1});
 
 });
