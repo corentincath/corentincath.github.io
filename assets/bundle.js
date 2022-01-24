ad232beb25bbd6433f07b36085f299d67963f17b
Corentin <aazertypher@gmail.com>
2022-01-24 14:30:22 +0100

    Test 1 gsap

--- assets/bundle.js
+++ assets/bundle.js
@@ -7,5 +7,6 @@ import gsap from 'gsap';
 $(document).ready(function() {
     var title_span = document.querySelectorAll('h1 span');
 
+    gsap.to(title_span[0], {transform: "translateY(10px)"});
     console.log(title_span)
 });
